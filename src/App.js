9a9f4baac70abbc74bd99bfddbcbe31032ad3008
eric capiz <ericcapiz@gmail.com>
2020-09-23 20:18:45 -0500

    productcontext created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
+import {ProductContext} from './context/ProductContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
+
 
 function App() {
 	const [products] = useState(data);
@@ -17,16 +19,18 @@ function App() {
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={prodcuts,addItem}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
